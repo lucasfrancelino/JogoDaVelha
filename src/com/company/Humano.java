9353a23d4bf20fbc4f64b6b4195a597dd97f1c25
Lucas Francelino Silva de Sousa <lucas.sousa@engesoftware.com.br>
2019-08-01 19:03:35 -0300

    Jogo feito para treinar a lógica e todo conteudo estudade no java básico

---
+++ src/com/company/Humano.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Humano extends Jogador {
+
+    public Scanner entrada = new Scanner(System.in);
+
+    public Humano(int jogador){
+        super(jogador);
+        this.jogador = jogador;
+        System.out.println("Jogador 'Humano' criado!");
+    }
+
+    @Override
+    public void jogar(Tabuleiro tabuleiro) {
+        Tentativa(tabuleiro);
+        tabuleiro.setPosicao(tentativa, jogador);
+    }
+
+    @Override
+    public void Tentativa(Tabuleiro tabuleiro) {
+        do{
+            do{
+                System.out.println("Linha: ");
+                tentativa[0] = entrada.nextInt();
+
+                if(tentativa[0] > 3 || tentativa[0] < 1)
+                    System.out.println("Tentativa invalida. É 1,2 ou 3");
+            }while(tentativa[0] > 3 || tentativa[0] < 1);
+
+            do{
+                System.out.println("Coluna: ");
+                tentativa[1] = entrada.nextInt();
+
+                if(tentativa[1] > 3 || tentativa[1] < 1)
+                    System.out.println("Coluna inválida. É 1, 2 ou 3");
+            }while(tentativa[1] >3 || tentativa[1] < 1);
+
+            tentativa[0]--;
+            tentativa[1]--;
+
+            if(!checarTentativa(tentativa, tabuleiro))
+                System.out.println("Esse local já foi marcado. Tente outro.");
+        }while(!checarTentativa(tentativa, tabuleiro));
+    }
+}
---
+++ src/com/company/JogoDaVelha.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class JogoDaVelha {
+
+    public static void main(String[] args) {
+
+//        exercicio feito com o auxilio do site:
+//        http://www.javaprogressivo.net/2012/10/Como-fazer-um-Jogo-da-Velha-em-Java.html
+
+        Jogo jogo = new Jogo();
+
+    }
+}
